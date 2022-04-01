36fa1001fe9b99e1b5c2aa0a471f5790e2dd57bb
AntonSachuk <sachuk.aa@intechs.by>
2022-04-01 11:19:51 +0300

    correct fix message

--- apps/email/send_email.py
+++ apps/email/send_email.py
@@ -11,7 +11,7 @@ async def send_email(email: EmailSchema, params: dict):
         <div style="margin: 0 auto; width: 90%; text-align: center;">
             <h1 style="background-color: rgba(0, 53, 102, 1); padding: 5px 10px; border-radius: 5px; color: white;">{params['title']}</h1>
             <div style="margin: 30px auto; background: white; width: 40%; border-radius: 10px; padding: 50px; text-align: center;">
-                <h3 style="margin-bottom: 100px; font-size: 24px;">{params['name']}!</h3>
+                <h3 style="margin-bottom: 100px; font-size: 24px;">MESSAGE FROM {params['name']}!</h3>
                 <p style="margin-bottom: 30px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
                     doloremque.</p>
                 <a style="display: block; margin: 0 auto; border: none; background-color: rgba(255, 214, 10, 1); color: white; width: 200px; line-height: 24px; padding: 10px; font-size: 24px; border-radius: 10px; cursor: pointer; text-decoration: none;"
@@ -25,6 +25,7 @@ async def send_email(email: EmailSchema, params: dict):
     </div>
     </body>
     </html>"""
+
     message = MessageSchema(
         subject="REPORT FROM SOFIT",
         recipients=email.dict().get("email"),  # List of recipients, as many as you can pass
