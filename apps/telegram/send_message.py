62681861eca03df8b38b88f6b3b88dc218ff47a5
raziel <sachuk.aa@intechs.by>
2022-04-07 16:07:39 +0300

    correct

--- apps/telegram/send_message.py
+++ apps/telegram/send_message.py
@@ -8,6 +8,6 @@ api_telegram = f"https://api.telegram.org/bot{token}"
 send_message_url = f"{api_telegram}/sendDocument"
 
 
-async def send_report_to_telegram(chat_id: str, payload: str) -> JSONResponse:
+async def send_message(chat_id: str, payload: str) -> JSONResponse:
     response = httpx.post(send_message_url, data={'chat_id': chat_id, 'document': payload})
     # return JSONResponse(status_code=200, content={"message": 'File send to telegram channel'})
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import os
 from typing import List
-from fastapi import FastAPI, Query, Request
+from fastapi import FastAPI, Query
 from starlette.responses import JSONResponse
 
-from apps.Telegram.send_message import send_report_to_telegram
 from apps.email.schemas import EmailSchema
 from apps.email.send_email import send_email
-
-users = {
-    1: {'first_name': 'test1', 'last_name': 'test1', 'how_send': 'email'},
-    2: {'first_name': 'test2', 'last_name': 'test2', 'how_send': 'telegram'},
-    3: {'first_name': 'test3', 'last_name': 'test3', 'how_send': 'email'},
-    4: {'first_name': 'test4', 'last_name': 'test4', 'how_send': 'telegram'},
-}
+from apps.telegram.send_message import send_message
 
 app = FastAPI()
 
@@ -28,7 +21,6 @@ async def send_report_asynchronous(
         users_id: List[str] = Query(None, description='Send report for users'),
         payload: str = Query(None, description='Link to file. This field required')) \
         -> JSONResponse:
-
-    # await send_report_to_telegram(chat_id='-1001576623026', payload=f"{payload}")
+    # await send_message(chat_id='-1001576623026', payload=f"{payload}")
     await send_email(EmailSchema(email=[os.getenv('MY_EMAIL')]), params={'title': 'REPORT', 'name': 'QWERTY'})
     return JSONResponse(status_code=200, content={"message": "email has been sent"})
